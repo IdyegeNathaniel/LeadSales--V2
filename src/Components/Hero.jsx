532c5733f05d34091c9116854c6f110e561fc4e4
Idyege Nathaniel <idyegenathaniel@gmail.com>
2024-09-25 00:04:25 +0100

    HERO BACKGROUND

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,14 +4,14 @@ const Hero = ({
   title = "We Are Your Realtors!",
   subtitle = "Buy And Sell Your Properties Here",
 }) => {
-  ///backgeound for hero component
-  // const backGround = {
-  //   backgroundImage: "url('src/assets/images/BG.jpg')",
-  //   backgroundSize: "cover",
-  //   backgroundPosition: "center",
-  //};
+  //backgeound for hero component
+   const backGround = {
+     backgroundImage: "url('src/assets/images/BG.jpg')",
+     backgroundSize: "cover",
+     backgroundPosition: "center",
+  };
   return (
-    <div className="w-full py-40 mb-4 bg-orange-500 border-b border-b-orange-400">
+    <div className="w-full py-40 mb-4 border-b border-b-orange-400" style={backGround}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
         <div className="text-center mt-10 text-white">
           <h1 className="text-4xl font-extrabold sm:text-5xl md:text-6xl">
