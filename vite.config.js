aa6ba3cef2a13e19c2e6119df19f1fb5f091bb37
Idyege Nathaniel <idyegenathaniel@gmail.com>
2024-10-22 00:23:50 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://properties-api-ruddy.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
